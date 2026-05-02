85811520023d10717ebd0ea45ac5f0ab396c03c9
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2026-05-02 18:09:13 +0200

    Initial monorepo with microservices

---
+++ snippet-service/src/test/java/com/codecollab/snippet_service/SnippetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecollab.snippet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
