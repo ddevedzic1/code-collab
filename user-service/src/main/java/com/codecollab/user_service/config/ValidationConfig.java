10bda3cac83af643f971c830182b0c2fc2f4eb02
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2026-05-10 14:21:06 +0200

    Implement ValidationConfig for localization

---
+++ user-service/src/main/java/com/codecollab/user_service/config/ValidationConfig.java
@@ -0,0 +1,22 @@
+package com.codecollab.user_service.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ValidationConfig {
+
+	private final MessageSource messageSource;
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		var validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(messageSource);
+		return validator;
+	}
+}
